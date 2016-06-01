7870da08f75c023e9103830c9a42228210459300
Joshua Lynch <jl52741@gmail.com>
2016-06-01 16:40:09 -0500

    call caf where needed :)

--- BoardConfigRaindrop.mk
+++ BoardConfigRaindrop.mk
@@ -1,3 +1,10 @@
+# Display
+TARGET_QCOM_DISPLAY_VARIANT := caf-msm8916
+
+# Audio/media
+TARGET_QCOM_AUDIO_VARIANT := caf-msm8916
+TARGET_QCOM_MEDIA_VARIANT := caf-msm8916
+
 # Custom lun
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
