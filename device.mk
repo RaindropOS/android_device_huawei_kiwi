8a12d12575f42d749ca4567ca35fbf042989bbab
Joshua Lynch <jl52741@gmail.com>
2016-06-01 16:51:15 -0500

    Look at caf for this

--- device.mk
+++ device.mk
@@ -53,8 +53,8 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1280
 
-$(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
+$(call inherit-product, frameworks/native-caf/build/phone-hdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native-caf/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
